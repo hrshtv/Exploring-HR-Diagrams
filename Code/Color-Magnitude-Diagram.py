908f63e58553784bfeb924da1e64e173eba3791d
Harshit <varmaharshit2@gmail.com>
2020-06-22 20:27:13 +0530

    Updated README.md
    TODO: Add references

--- Code/Color-Magnitude-Diagram.py
+++ Code/Color-Magnitude-Diagram.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 # Input from user
 clusName = input("Enter the numeric part of the cluster's name  : ")
 name = "NGC"+clusName
-fname = "DATA/NGC"+clusName+".txt" 
+fname = "../DATA/NGC"+clusName+".txt"
 
 x1 = np.loadtxt(fname, usecols=(2))
 x2 = np.loadtxt(fname, usecols=(8))
@@ -14,7 +14,7 @@ prob = np.loadtxt(fname, usecols=(32))
 # Removing garbage & including only the stars which have more than 90% membership probability
 b = (x1 > 0) & (x2 > 0) & (prob>90)
 y = x1[b]
-x = x1[b] - x2[b] 
+x = x1[b] - x2[b]
 
 # The plot customization
 factor = 0.0001
@@ -25,10 +25,10 @@ fig = plt.figure()
 ax = fig.add_subplot(1, 1, 1) # nrows, ncols, index
 ax.set_facecolor([0,0,0])
 
-# The plot 
+# The plot
 plt.scatter(x, y, s = area, c = colors)
 plt.title(name, fontsize = 20, fontweight="bold")
-plt.ylabel("Apparent Magnitude", fontsize = 18) 
+plt.ylabel("Apparent Magnitude", fontsize = 18)
 plt.xlabel("Color Index", fontsize = 18)
 
 # Inverting the y-axis and setting the limits, so that correlation with HRD is easier as HRD plots luminosity increasing along the y - axis
@@ -36,6 +36,7 @@ plt.gca().invert_yaxis()
 # This ensures that the aspect ratio is 1:1, as in the archived file, i,e the image looks 'square'
 plt.gca().set_aspect(aspect = (abs(np.amax(x)-np.amin(x)))/abs(np.amax(y)-np.amin(y)))
 #The limits on the axes
-plt.xlim(np.amin(x), np.amax(x)) 
-plt.ylim(np.amax(y), np.amin(y)) 
+plt.xlim(np.amin(x), np.amax(x))
+plt.ylim(np.amax(y), np.amin(y))
+# plt.savefig('../img/cmd_ngc2808.png', dpi=300)
 plt.show()
--- README.md
+++ README.md
@@ -1,7 +1,15 @@
 # Exploring Hertzsprung Russell Diagrams  
-Winter project taken up in 2019,  under [Krittika - Astronomy Club of IIT Bombay](https://www.facebook.com/KrittikaIITB/) ([@krittikaiitb](https://github.com/krittikaiitb))
+<p align="center"> 
+    <img src="img/cmd_ngc2808.png">
+</p>
+
+
+
+Winter project taken up in 2019,  under [Krittika - Astronomy Club of IIT Bombay](https://www.facebook.com/KrittikaIITB/) ([@krittikaiitb](https://github.com/krittikaiitb)).     
+
+Most data was taken from [HST UV Globular Cluster Survey ("HUGS")](https://archive.stsci.edu/prepds/hugs/).
 
 ---
 
-- [Related code](Exploring-HR-Diagrams/Code)
-- [Project report and presentation slides](Exploring-HR-Diagrams/Reports)
+- [Related code](Code)
+- [Project report and presentation slides](Reports)
